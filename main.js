9ff89bc318456c865a81067e243b5c9d262ce1a5
Fortes.Huang <fortes.huang@smartclouds.info>
2023-03-30 18:20:42 +0800

    feat: add rem.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-import './style.css'
+import "./style.scss";
+import setRootFontSize from "./rem.js";
+
+function debounce(func, wait) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  };
+}
+
+document.addEventListener("DOMContentLoaded", () =>
+  setRootFontSize(document, window)
+);
+
+const debouncedSetRootFontSize = debounce(
+  () => setRootFontSize(document, window),
+  10
+);
+window.addEventListener("resize", debouncedSetRootFontSize);
+window.addEventListener("orientationchange", debouncedSetRootFontSize);
